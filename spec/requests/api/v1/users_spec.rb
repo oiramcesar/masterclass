8077b3837e4913b6065a1f59aeaad5bd8ac67360
oiramcesar <oiramcesar@yahoo.com.br>
2020-08-11 23:06:32 +0000

    Adding tests to routes, action:create and implementings

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -6,12 +6,11 @@ RSpec.describe 'Users API', type: :request do
 
     before { host! 'api.taskmanager.test:3000'}
 
-    describe 'GET users/:id' do
+    describe 'GET /users/:id' do
         before do
-            headers = {'Accept'=>'application/vnd.taskmanager.v1'}
-            body = {}
+            headers = {'Accept' => 'application/vnd.taskmanager.v1'}
             # get "/users/#{user_id}", :params => {}, :headers=> headers ou
-            get "/users/#{user_id}", headers: headers, params: body
+            get "/users/#{user_id}", headers: headers, params: {}
         end
 
         context 'when the user exists' do
@@ -31,6 +30,41 @@ RSpec.describe 'Users API', type: :request do
             it 'returns status code 404' do
                 expect(response).to have_http_status(404)
             end
+        end
+
+    end
+
+    describe 'POST /users' do
+        before do
+            headers = {'Accept'=> 'application/vnd.taskmanager.v1'}
+            post '/users', params:{user: user_params}, headers: headers
+        end
+
+        context 'when the request params are valids' do
+            let(:user_params){ attributes_for(:user)}
+
+            it 'returns status code 201' do
+                expect(response).to have_http_status(201)
+            end
+
+            it 'return JSON data for created user' do
+                user_response = JSON.parse(response.body)
+                expect(user_response['email']).to eq(user_params[:email])
+            end
+        end
+
+        context 'when the request params are invalids' do
+            let(:user_params){ attributes_for(:user, email: 'Invalid_email@')}
+
+            it 'returns status code 422' do
+                expect(response).to have_http_status(422)
+            end
+
+            it 'return JSON data with errors' do
+                user_response = JSON.parse(response.body)
+                expect(user_response).to have_key('errors')
+            end
+
         end
 
     end
