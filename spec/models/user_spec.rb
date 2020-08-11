d653b7983cbb810ae10f154a9675ecc87d63dc96
oiramcesar <oiramcesar@yahoo.com.br>
2020-08-11 10:08:08 +0000

    Adding shoulda-matcher at spec users

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  before { @user = FactoryBot.build(:user) }
 
-  it { expect(@user).to respond_to(:email)}
-  it { expect(@user).to respond_to(:name)}
-  it { expect(@user).to respond_to(:password)}
-  it { expect(@user).to respond_to(:password_confirmation)}
-  it { expect(@user).to be_valid}
+  let(:user1) { build(:user) }
+
+  it { is_expected.to validate_presence_of(:email) }
+  it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
+  it { is_expected.to validate_confirmation_of(:password) }
+  it { is_expected.to allow_value("mario@teste.com").for(:email) }
+
 end
