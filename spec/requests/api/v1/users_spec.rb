8aeb3efa173cb2411eaad8d06d133b2ffef382dc
oiramcesar <oiramcesar@yahoo.com.br>
2020-08-12 00:30:44 +0000

    Adding tests of update, delete of users and implementing

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -69,4 +69,54 @@ RSpec.describe 'Users API', type: :request do
 
     end
 
+    describe 'PUT /users/:id' do
+        before do
+            headers = {'Accept' => 'application/vnd.taskmanager.v1'}
+            put "/users/#{user_id}", headers: headers, params:{user: user_params}
+        end
+
+        context 'when the request params are valid' do
+            let(:user_params) { { email: 'new_email@taskmanager.com' } }
+            
+            it 'returns status code 200' do
+                expect(response).to have_http_status(200)
+            end
+
+            it 'return JSON data with update information' do
+                user_response = JSON.parse(response.body, symbolize_names: true)
+                expect(user_response[:email]).to eq(user_params[:email])
+            end
+
+        end
+        
+        context 'when the request params are invalid' do
+            let(:user_params) { { email: 'New_email@' } }
+            
+            it 'returns status code 422' do
+                expect(response).to have_http_status(422)
+            end
+
+            it 'return JSON data with errors' do
+                user_response = JSON.parse(response.body, symbolize_names: true)
+                expect(user_response).to have_key(:errors)
+            end
+        end
+    end
+
+    describe 'DELETE /users/:id' do
+        before do
+            headers = {'Accept'=> 'application/vnd.taskmanager.v1'}
+            delete "/users/#{user_id}", params:{}, headers: headers
+        end
+
+        it 'returns status code 204' do
+            expect(response).to have_http_status(204)
+        end
+
+        it 'removes the user from database' do
+            expect(User.find_by(id: user.id)).to be_nil
+        end
+
+    end
+
 end
