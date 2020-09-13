dfc55c1b8414d0c9bbeed09879be8d354ba1ecbf
oiramcesar <oiramcesar@yahoo.com.br>
2020-09-13 14:12:48 +0000

    Fixing error in short_descripition at task_serializer

--- app/serializers/api/v2/task_serializer.rb
+++ app/serializers/api/v2/task_serializer.rb
@@ -4,7 +4,7 @@ class Api::V2::TaskSerializer < ActiveModel::Serializer
   attributes :id, :title, :description, :done, :deadline, :user_id, :created_at, :updated_at, :short_description, :is_late
 
   def short_description
-    object.description[0..10]
+    object.description[0..10] if object.description.present?
   end
 
   def is_late
